7d5c3158e1412f2eef7616ed63a7908ebbc4c44c
Alexander Jenne <alexander.jenne@minebea-intec.com>
2023-12-07 11:48:19 +0100

    WIP navigation

---
+++ vino-ui/src/layout/navigation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <h1>Navigation</h1>
+      <img src="vinoVibes.png" />
+    </div>
+  );
+};
+
+export default Navigation;
