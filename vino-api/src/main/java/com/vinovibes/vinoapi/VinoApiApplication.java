6b8121e3e2e0b448825368bf27e29e8c24e9c1ad
Adrian Hitsch <adrian.hitsch@outlook.de>
2023-12-12 20:14:26 +0100

    Init npm and configure prettier for java

--- vino-api/src/main/java/com/vinovibes/vinoapi/VinoApiApplication.java
+++ vino-api/src/main/java/com/vinovibes/vinoapi/VinoApiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VinoApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VinoApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(VinoApiApplication.class, args);
+    }
 }
---
+++ vino-api/src/main/java/com/vinovibes/vinoapi/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.vinovibes.vinoapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
