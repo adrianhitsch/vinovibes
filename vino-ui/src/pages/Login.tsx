54c7ed08f7fb35a2d3da701fd8049aff3c52b27d
Alexander Jenne <alexander.jenne@minebea-intec.com>
2023-12-14 12:50:24 +0100

    Add login functionality

--- vino-ui/src/pages/Login.tsx
+++ vino-ui/src/pages/Login.tsx
@@ -1,7 +1,67 @@
 import React from 'react';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import config from '../config';
+import { useNavigate } from 'react-router';
 
 const Login = (): JSX.Element => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const login = async (e: any) => {
+    const name = document.getElementById('email') as HTMLInputElement;
+    const password = document.getElementById('password') as HTMLInputElement;
+
+    await fetch(`${config.API_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: name,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    console.log('login');
+    navigate('/');
+  };
+
+  return (
+    <div className="login">
+      <img src="login-image.png" alt="login-image" className="login-image" />
+      <div className="login-container">
+        <div className="logo">
+          <img src="vinoVibes-light.png" alt="VinoLogo" className="vino-logo" />
+          <h1>VinoVibes</h1>
+        </div>
+        <p>VinoVibes ist keine Website. Es ist ein Lifestyle!</p>
+
+        <div className="group">
+          <InputText type="email" name="email" id="email" placeholder="E-mail" />
+        </div>
+        <div className="group">
+          <InputText type="password" name="password" id="password" placeholder="Passwort" />
+        </div>
+        <div className="group">
+          <Button label="Passwort vergessen?" className="button transparent text" />
+        </div>
+        <div className="group">
+          <Button label="Login" onClick={login} className="button" />
+        </div>
+        <div className="group">
+          <p>Noch keinen Account?</p>
+          <Button label="Jetzt registrieren" onClick={login} className="button secondary" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
