6bd434573a341630289cea66a72ea0d6e29f77f6
Cornelius Köpper <cornelius.koepper@arineo.com>
2024-01-25 11:09:15 +0100

    Refactor controller mappings to use "/api" prefix

--- vino-api/src/main/java/com/vinovibes/vinoapi/controller/AuthController.java
+++ vino-api/src/main/java/com/vinovibes/vinoapi/controller/AuthController.java
@@ -12,13 +12,11 @@ import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class AuthController {
 
     private final UserFacade userFacade;
